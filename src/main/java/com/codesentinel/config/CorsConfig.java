2915b6cbd31e6e4445eae47a30571a7a0853fd02
MINHTUAN\minht <minhtuan7543@gmail.com>
2026-07-25 01:34:37 +0700

    Fix corg

--- src/main/java/com/codesentinel/config/CorsConfig.java
+++ src/main/java/com/codesentinel/config/CorsConfig.java
@@ -2,11 +2,7 @@ package com.codesentinel.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.*;
 
 import java.util.List;
 
@@ -16,30 +12,29 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
-        CorsConfiguration configuration =
-                new CorsConfiguration();
+        CorsConfiguration config = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
-        );
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://codesentinal-cagrbwolv-nguyenhuuminhtuans-projects.vercel.app"
+        ));
 
-        configuration.setAllowedMethods(
-                List.of("*")
-        );
+        config.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
 
-        configuration.setAllowedHeaders(
-                List.of("*")
-        );
+        config.setAllowedHeaders(List.of("*"));
 
-        configuration.setAllowCredentials(true);
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
-        source.registerCorsConfiguration(
-                "/**",
-                configuration
-        );
+        source.registerCorsConfiguration("/**", config);
 
         return source;
     }
